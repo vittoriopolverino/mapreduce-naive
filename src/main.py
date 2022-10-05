e726ec37590845ab056f1953181bd602bf7a1de5
Vittorio Polverino <vittorio.polverino@brumbrum.it>
2022-10-05 19:19:26 +0200

    moved main file in src folder

--- src/main.py
+++ src/main.py
@@ -17,22 +17,19 @@ def shuffler(map_result: list[(str, int)]) -> dict[str, list[int]]:
     shuffler = defaultdict(list)
     for word, occurrence in map_result:
         shuffler[word].append(occurrence)
-
     return shuffler
 
 
-def reducer(shuffle_result: dict[str, list[int]]) -> list[tuple[str, int]]:
+def reducer(shuffle_result: dict[str, list[int]]) -> list[(str, int)]:
     occurrences = [sum(occurrence) for word, occurrence in shuffle_result.items()]
     # print(occurrences)
-
     reducer = lambda word, occurrence: (word, occurrence)
     return list(map(reducer, shuffle_result, occurrences))
 
 
-def map_reduce_naive(sentence: str) -> list[tuple[str, int]]:
+def map_reduce_naive(sentence: str) -> list[(str, int)]:
     map_result = mapper(sentence=sentence)
     # print(map_result)
-
     shuffle_result = shuffler(map_result=map_result)
     # print(shuffle_result.items())
     return reducer(shuffle_result=shuffle_result)
@@ -40,7 +37,7 @@ def map_reduce_naive(sentence: str) -> list[tuple[str, int]]:
 
 if __name__ == '__main__':
     # import data
-    with open('example.txt') as file:
+    with open('../example.txt') as file:
         sentence = file.read().lower()
 
     # get the start time
