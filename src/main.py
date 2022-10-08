69d55efc20eee1da7f0f0ea4996df6ff3414b726
Vittorio Polverino <vittorio.polverino@brumbrum.it>
2022-10-08 21:02:05 +0200

    code refactoring

--- src/main.py
+++ src/main.py
@@ -1,61 +1,22 @@
-from collections import defaultdict
-import time
 import operator
+import time
 
+import word_count as wdc
 
-def split_sentence(sentence: str) -> list[str]:
-    return sentence.split()
+if __name__ == "__main__":
+    with open("example.txt") as file:  # import data
+        file_text = file.read()
 
+    st = time.time()  # get the start time
 
-def mapper(sentence: str) -> list[(str, int)]:
-    words = split_sentence(sentence=sentence)
-    mapper = lambda word: (word, 1)
-    return list(map(mapper, words))
+    result = wdc.map_reduce_naive(text=file_text)
+    result.sort(
+        key=operator.itemgetter(1), reverse=True
+    )  # descending sort based on the number of occurrences
 
+    et = time.time()  # get the end time
 
-def shuffler(map_result: list[(str, int)]) -> dict[str, list[int]]:
-    shuffler = defaultdict(list)
-    for word, occurrence in map_result:
-        shuffler[word].append(occurrence)
-    return shuffler
+    elapsed_time = et - st  # get the execution time
+    print("Execution time:", elapsed_time, "seconds")
 
-
-def reducer(shuffle_result: dict[str, list[int]]) -> list[(str, int)]:
-    occurrences = [sum(occurrence) for word, occurrence in shuffle_result.items()]
-    # print(occurrences)
-    reducer = lambda word, occurrence: (word, occurrence)
-    return list(map(reducer, shuffle_result, occurrences))
-
-
-def map_reduce_naive(sentence: str) -> list[(str, int)]:
-    map_result = mapper(sentence=sentence)
-    # print(map_result)
-    shuffle_result = shuffler(map_result=map_result)
-    # print(shuffle_result.items())
-    return reducer(shuffle_result=shuffle_result)
-
-
-if __name__ == '__main__':
-    # import data
-    with open('../example.txt') as file:
-        sentence = file.read().lower()
-
-    # get the start time
-    st = time.time()
-
-    result = map_reduce_naive(sentence=sentence)
-
-    # descending sort based on the number of occurrences
-    result.sort(key=operator.itemgetter(1), reverse=True)
-
-    # get the end time
-    et = time.time()
-
-    # get the execution time
-    elapsed_time = et - st
-    print('Execution time:', elapsed_time, 'seconds')
-
-    # top 10
-    print(result[:10])
-
-
+    print(result[:10])  # top 10
--- src/word_count.py
+++ src/word_count.py
@@ -1,30 +1,29 @@
 from collections import defaultdict
 
 
-def mapper(sentence: str) -> list[(str, int)]:
-    words = split_sentence(sentence=sentence)
-    mapper = lambda word: (word, 1)
-    return list(map(mapper, words))
+def mapper(text: str) -> list[(str, int)]:
+    words = text.lower().split()
+    return list(map(lambda word: (word, 1), words))
 
 
-def shuffler(map_result: list[(str, int)]) -> dict[str, list[int]]:
-    shuffler = defaultdict(list)
-    for word, occurrence in map_result:
-        shuffler[word].append(occurrence)
-    return shuffler
+def shuffler(mapped_data: list[(str, int)]) -> dict[str, list[int]]:
+    word_occurrences = defaultdict(list)
+    for word, occurrence in mapped_data:
+        word_occurrences[word].append(occurrence)
+    return word_occurrences
 
 
-def reducer(shuffle_result: dict[str, list[int]]) -> list[(str, int)]:
-    occurrences = [sum(occurrence) for word, occurrence in shuffle_result.items()]
+def reducer(shuffled_data: dict[str, list[int]]) -> list[(str, int)]:
+    occurrences = [sum(occurrence) for word, occurrence in shuffled_data.items()]
     # print(occurrences)
-    reducer = lambda word, occurrence: (word, occurrence)
-    return list(map(reducer, shuffle_result, occurrences))
+    return list(
+        map(lambda word, occurrence: (word, occurrence), shuffled_data, occurrences)
+    )
 
 
-def map_reduce_naive(sentence: str) -> list[(str, int)]:
-    map_result = mapper(sentence=sentence)
-    # print(map_result)
-    shuffle_result = shuffler(map_result=map_result)
-    # print(shuffle_result.items())
-
-    return reducer(shuffle_result=shuffle_result)
+def map_reduce_naive(text: str) -> list[(str, int)]:
+    mapping_result = mapper(text=text)
+    # print(mapping_result)
+    shuffling_result = shuffler(mapped_data=mapping_result)
+    # print(shuffling_result.items())
+    return reducer(shuffled_data=shuffling_result)
